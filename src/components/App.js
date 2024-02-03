10cf237acec242e999d10021e2d44ba2e0c56a87
Sainath <71361447+sainath161@users.noreply.github.com>
2024-02-03 17:02:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <form onSubmit = "{handleSubmit}">
+          <label>
+            Enter your name:
+            <input type = "text" value = {userName} onChange = {handleNameChange} />
+          </label>
+          <button type = "submit"> Submit </button>
+        </form>
+      {submitted && ( 
+        <p>
+          Hello {userName}!
+        </p>
+      )}
     </div>
   )
 }
